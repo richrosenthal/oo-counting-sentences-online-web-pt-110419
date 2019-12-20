fe8b8be8464f1f820cfe4387750b30b8ca066e03
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-20 17:21:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else 
+      return false
+    end 
   end
 
   def exclamation?
-
+   if self.end_with?("!")
+     return true
+    else 
+      return false 
+    end 
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/)
+   
+    
+    if new_array.length == 5
+       new_array.pop
+    end 
+    new_array.length 
   end
 end
